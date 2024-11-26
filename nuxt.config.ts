9b80c35d3d8050a39200909c07a32671e8bac311
nabeel-sadiq <nabeelsadiq.dev@gmail.com>
2024-11-26 10:29:28 +0500

    added favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  app: {
+    //head links
+    head: {
+      link: [
+        //favicon
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+      ]
+    }
+  }
 })
